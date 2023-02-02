7cdc2a09cc6337c2dd2eba98705a39dbc730f915
unknown <benjaminfkile@gmail.com>
2023-02-01 18:43:09 -0700

    bump2

--- src/components/services/serviceItems.tsx
+++ src/components/services/serviceItems.tsx
@@ -1,7 +1,7 @@
 const serviceItems = [
     {
-        icon: "local_shipping",
-        title: "we come to you",
+        icon: "recycling",
+        title: "junk recycling",
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."
     },
     {
